7e8d9cb83c4ae70de68ba20c8328b9add804d897
Tefany24 <stephanie.figueroa2026@hilasal.superate.org.sv>
2025-02-20 22:01:34 -0600

    añadi la función de adregar un platillo al pedido

--- proyecto.py
+++ proyecto.py
@@ -14,3 +14,16 @@ def mostrar_menu():
     print("\nMenú del Restaurante:")
     for i, platillo in enumerate(menu):
         print(f"{i + 1}. {platillo['nombre']} - ${platillo['precio']}")
+
+        # Función para agregar un platillo al pedido
+def agregar_pedido(pedido):
+    mostrar_menu()
+    try:
+        opcion = int(input("\nSelecciona el número del platillo que deseas agregar: ")) - 1
+        if 0 <= opcion < len(menu):
+            pedido.append(menu[opcion])
+            print(f"Agregado: {menu[opcion]['nombre']} - ${menu[opcion]['precio']}")
+        else:
+            print("Opción no válida.")
+    except ValueError:
+        print("Por favor, ingresa un número válido.")
